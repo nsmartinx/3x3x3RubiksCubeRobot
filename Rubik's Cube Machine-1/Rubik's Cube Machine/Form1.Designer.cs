94810a6966b9c800ea324dacea0f2422aacb4dd1
nsmartinx <98997863+nsmartinx@users.noreply.github.com>
2024-02-07 23:30:21 -0500

    Add files via upload

---
+++ Rubik's Cube Machine-1/Rubik's Cube Machine/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace Rubik_s_Cube_Machine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboCamera = new System.Windows.Forms.ComboBox();
+            this.picCamera = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.findCamera = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picCamera)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cboCamera
+            // 
+            this.cboCamera.FormattingEnabled = true;
+            this.cboCamera.Location = new System.Drawing.Point(84, 21);
+            this.cboCamera.Name = "cboCamera";
+            this.cboCamera.Size = new System.Drawing.Size(265, 21);
+            this.cboCamera.TabIndex = 1;
+            // 
+            // picCamera
+            // 
+            this.picCamera.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picCamera.Location = new System.Drawing.Point(12, 48);
+            this.picCamera.Name = "picCamera";
+            this.picCamera.Size = new System.Drawing.Size(486, 406);
+            this.picCamera.TabIndex = 2;
+            this.picCamera.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(423, 492);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // findCamera
+            // 
+            this.findCamera.Location = new System.Drawing.Point(19, 492);
+            this.findCamera.Name = "findCamera";
+            this.findCamera.Size = new System.Drawing.Size(92, 23);
+            this.findCamera.TabIndex = 4;
+            this.findCamera.Text = "Find Cameras";
+            this.findCamera.UseVisualStyleBackColor = true;
+            this.findCamera.Click += new System.EventHandler(this.findCamera_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Camera:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(545, 258);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Colour";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(591, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Get Colour";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 542);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.findCamera);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.picCamera);
+            this.Controls.Add(this.cboCamera);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picCamera)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox cboCamera;
+        private System.Windows.Forms.PictureBox picCamera;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button findCamera;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
