35ee33f49ee0d93112d146958aea655a4139688f
nsmartinx <98997863+nsmartinx@users.noreply.github.com>
2024-02-07 23:32:39 -0500

    Add files via upload

---
+++ Rubik'sCubeSolverV3/Rubik'sCubeSolverV3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rubik_sCubeSolverV3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
